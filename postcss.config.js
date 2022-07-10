520f4f1037b53ec0183a3b426be8c72fa8408625
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-10 20:55:30 +0900

    postcss.confing.js 수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
 module.exports = {
     parser: "postcss-scss",
-    options: {
-        sourceMap: true,
-    },
     plugins: [
-        "postcss-flexbugs-fixes",
-        "autoprefixer",
-        "postcss-fail-on-warn",
+        require("postcss-flexbugs-fixes"),
+        require("autoprefixer"),
+        require("postcss-fail-on-warn"),
     ],
 };
