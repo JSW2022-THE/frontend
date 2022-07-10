99bc6a842758fc70cb7232fa704d07ac43dd7d59
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-10 21:54:41 +0900

    BottomNavigation 메뉴 추가

--- components/BottomNavigation.js
+++ components/BottomNavigation.js
@@ -1,9 +1,64 @@
 import styles from '../styles/components/BottomNavigation/BottomNavigation.module.css'
+import {IoMdHome} from "react-icons/io";
+import classNames from "classnames";
 
 const BottomNavigation = (props) => {
     return (
         <>
             <div className={styles.bottom_navigation}>
+                {/* 메뉴 갯수에 따라 scss 파일에서 width 길이 나누기 변경할 것 (메뉴 갯수로) */}
+                <div className={styles.menu}>
+                    <div className={classNames(styles.menu_box, styles.menu_active)}>
+                        <div className={styles.menu_center}>
+                            <div className={styles.menu_icon}>
+                                <IoMdHome />
+                            </div>
+                            <div className={styles.menu_name}>
+                                홈
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.menu_box}>
+                        <div className={styles.menu_center}>
+                            <div className={styles.menu_icon}>
+                                <IoMdHome />
+                            </div>
+                            <div className={styles.menu_name}>
+                                홈
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.menu_box}>
+                        <div className={styles.menu_center}>
+                            <div className={styles.menu_icon}>
+                                <IoMdHome />
+                            </div>
+                            <div className={styles.menu_name}>
+                                홈
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.menu_box}>
+                        <div className={styles.menu_center}>
+                            <div className={styles.menu_icon}>
+                                <IoMdHome />
+                            </div>
+                            <div className={styles.menu_name}>
+                                홈
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.menu_box}>
+                        <div className={styles.menu_center}>
+                            <div className={styles.menu_icon}>
+                                <IoMdHome />
+                            </div>
+                            <div className={styles.menu_name}>
+                                홈
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </>
