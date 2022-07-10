a190ccf294223ea86cd5a9485ee831d7cc915599
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-10 13:01:36 +0900

    BottomNavigation component 생성

---
+++ components/BottomNavigation.js
@@ -0,0 +1,13 @@
+import styles from '../styles/components/BottomNavigation/BottomNavigation.module.css'
+
+const BottomNavigation = (props) => {
+    return (
+        <>
+            <div className={styles.BottomNavigation}>
+
+            </div>
+        </>
+    )
+}
+
+export default BottomNavigation
