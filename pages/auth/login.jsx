1ca4cadf0dcdb0f46976d88f76f9d77aab708247
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-16 21:30:09 +0900

    로그인 페이지 추가

---
+++ pages/auth/login.jsx
@@ -0,0 +1,35 @@
+import Header from "../../components/Header";
+import styles from '../../styles/pages/auth/login/login.module.css'
+import kakaoLogin from '../../public/kakao_login.svg'
+import Image from 'next/image'
+import {useRouter} from 'next/router'
+
+export default function WorkerHome() {
+
+    const router = useRouter()
+    const kakaoLoginClick = () =>{
+        const REST_API_KEY = 'c91a830998b9123bba1465043c00c0cf'
+        const REDIRECT_URI = 'http://localhost:3000/auth/callback'
+        const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`
+        router.push(url)
+    }
+
+    return (
+        <>
+            <Header/>
+            <div className={styles.login_container}>
+                <div className={styles.login_box}>
+                    <div className={styles.big_text}>
+                        로그인
+                    </div>
+                    <div className={styles.small_text}>
+                        로그인 후 굿잡의 청소년 안전 서비스를 이용해보세요.
+                    </div>
+                    <div className={styles.kakao}>
+                        <Image src={kakaoLogin} alt="카카오로 로그인" onClick={kakaoLoginClick}/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
