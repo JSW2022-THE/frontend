4362bb2d40585668c1777bf482bd64617e7b4411
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-16 23:09:12 +0900

    네이밍 실수 수정
    WorkerHome -> Login

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -4,7 +4,7 @@ import kakaoLogin from '../../public/kakao_login.svg'
 import Image from 'next/image'
 import {useRouter} from 'next/router'
 
-export default function WorkerHome() {
+export default function Login() {
 
     const router = useRouter()
     const kakaoLoginClick = () =>{
