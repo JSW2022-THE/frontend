4d12dde5f29bc2a225f8cfca2b5554e86641653f
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-22 02:43:18 +0900

    로그인 완성!!!!

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import { Reset } from "styled-reset";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Reset />
       <div className='container'>
-        <Component {...pageProps} />
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <Component {...pageProps} />
+          </LocalizationProvider>
       </div>
     </>
   )
