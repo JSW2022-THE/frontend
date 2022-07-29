1672aa0aece39902113a3250e17a54a74729fcbf
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-30 05:08:21 +0900

    맵 로드 시 초기위치 설정(용암2동행정복지센터)

--- pages/worker/nearby.jsx
+++ pages/worker/nearby.jsx
@@ -22,7 +22,7 @@ export default function WorkerNearBy() {
       kakao.maps.load(() => {
         var el = document.getElementById("KakaoMap");
         var options = {
-          center: new kakao.maps.LatLng(33.450701, 126.570667),
+          center: new kakao.maps.LatLng(36.6072, 127.508),
           level: 5,
         };
         var map = new kakao.maps.Map(el, options);
