8ad3dcee139324804dfbf70a3bfa7500aee7f5df
HongRok Kim <hlog2e@gmail.com>
2022-08-01 01:01:18 +0900

    viewport meta tag _document.js 이슈 해결, 비동기 이슈 해결

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import { Reset } from "styled-reset";
-import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
-import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Reset />
-      <div className='container'>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <Component {...pageProps} />
-          </LocalizationProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className="container">
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Component {...pageProps} />
+        </LocalizationProvider>
       </div>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,34 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <script src="/css_error.js" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <link rel="stylesheet" as="style" crossOrigin="true"
-                        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/*
+            이거 땜에 오류남 수정좀.
+            <script src="/css_error.js" />
+            참고 링크 : https://nextjs.org/docs/messages/no-sync-scripts
+            */}
+          <link
+            rel="stylesheet"
+            as="style"
+            crossOrigin="true"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
