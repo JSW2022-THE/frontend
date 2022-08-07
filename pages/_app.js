6021a15acbc37d02ef6ead74c6f02e0d198dc9f8
HongRok Kim <hlog2e@gmail.com>
2022-08-08 07:42:41 +0900

    _app.js에 온라인 상태 체크 소켓 추가

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,41 @@ import { Reset } from "styled-reset";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import Head from "next/head";
-import {createTheme} from "@mui/material";
-import {ThemeProvider} from "@mui/styles";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@mui/styles";
+import { useEffect } from "react";
+import axios from "axios";
+import { io } from "socket.io-client";
 
 function MyApp({ Component, pageProps }) {
-    const THEME = createTheme({
-        typography: {
-            "fontFamily": "pretendard",
-            "fontSize": 16,
-            "fontWeightLight": 300,
-            "fontWeightRegular": 400,
-            "fontWeightMedium": 500
-        }
-    });
+  const THEME = createTheme({
+    typography: {
+      fontFamily: "pretendard",
+      fontSize: 16,
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+    },
+  });
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "http://localhost:2000/api/auth/getLoggedInUserUUID",
+      withCredentials: true,
+    })
+      .then((res) => {
+        const userUuid = res.data;
+        const socket = io("http://localhost:2000");
+
+        socket.on("connect", () => {
+          socket.emit("onlineChecker", userUuid);
+        });
+      })
+      .catch((err) => {
+        console.log("로그인이 안 되어 있는 상태임");
+      });
+  }, []);
   return (
     <>
       <Reset />
@@ -23,11 +45,11 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <ThemeProvider theme={THEME}>
-          <div className="container">
-              <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <Component {...pageProps} />
-              </LocalizationProvider>
-          </div>
+        <div className="container">
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Component {...pageProps} />
+          </LocalizationProvider>
+        </div>
       </ThemeProvider>
     </>
   );
