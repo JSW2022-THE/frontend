9e8653c2130092b82ceb394f6f6fee7d62398701
HongRok Kim <hlog2e@gmail.com>
2022-08-15 01:26:36 +0900

    Meta 테그 추가

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
           <link
             href="/icons/favicon-16x16.png"
             rel="icon"
